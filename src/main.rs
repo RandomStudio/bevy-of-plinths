86bc9b44fddacc067d7d2752858523fe846ede50
stephen <stephen.buchanan@random.studio>
2023-05-23 16:05:06 +0200

    camera can move up and down, too

--- src/main.rs
+++ src/main.rs
@@ -89,7 +89,7 @@ fn camera_control_system(
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    let (mut camera, mut camera_transform, camera_global_transform) = camera.single_mut();
+    let (_camera, mut camera_transform, _camera_global_transform) = camera.single_mut();
     let rotation = if input.pressed(KeyCode::Left) {
         time.delta_seconds()
     } else if input.pressed(KeyCode::Right) {
@@ -98,8 +98,20 @@ fn camera_control_system(
         0.0
     };
 
+    let movement = if input.pressed(KeyCode::Up) {
+        time.delta_seconds()
+    } else if input.pressed(KeyCode::Down) {
+        -time.delta_seconds()
+    } else {
+        0.0
+    };
+    let direction = camera_transform.local_y();
+    camera_transform.translation += direction * movement;
+
     camera_transform.rotate_around(
         Vec3::ZERO,
         Quat::from_euler(EulerRot::XYZ, 0.0, rotation, 0.0),
     );
+
+    camera_transform.look_at(Vec3::ZERO, Vec3::Y);
 }
