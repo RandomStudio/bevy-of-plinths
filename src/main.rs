6431e5ec8823b1073664e052ba387591caf25fd6
stephen <stephen.buchanan@random.studio>
2023-05-23 15:41:04 +0200

    lots of shiny balls

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,84 @@
-use bevy::prelude::*;
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+};
 
-#[derive(Component)]
-struct Person;
+use bevy::{
+    core_pipeline::{bloom::BloomSettings, tonemapping::Tonemapping},
+    prelude::*,
+};
 
-#[derive(Component)]
-struct Name(String);
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_startup_system(setup_scene)
         .run();
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
+fn setup_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Camera3dBundle {
+            camera: Camera {
+                hdr: true, // 1. HDR is required for bloom
+                ..default()
+            },
+            tonemapping: Tonemapping::TonyMcMapface, // 2. Using a tonemapper that desaturates to white is recommended
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        BloomSettings::default(), // 3. Enable bloom for the camera
+    ));
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
+    let material_emissive1 = materials.add(StandardMaterial {
+        emissive: Color::rgb_linear(13.99, 5.32, 2.0), // 4. Put something bright in a dark environment to see the effect
+        ..default()
+    });
+    let material_emissive2 = materials.add(StandardMaterial {
+        emissive: Color::rgb_linear(2.0, 13.99, 5.32),
+        ..default()
+    });
+    let material_emissive3 = materials.add(StandardMaterial {
+        emissive: Color::rgb_linear(5.32, 2.0, 13.99),
+        ..default()
+    });
+    let material_non_emissive = materials.add(StandardMaterial {
+        base_color: Color::GRAY,
+        ..default()
+    });
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
+    let mesh = meshes.add(
+        shape::Icosphere {
+            radius: 0.5,
+            subdivisions: 5,
         }
-    }
-}
+        .try_into()
+        .unwrap(),
+    );
 
-pub struct HelloPlugin;
+    for x in -10..10 {
+        for z in -10..10 {
+            let mut hasher = DefaultHasher::new();
+            (x, z).hash(&mut hasher);
+            let rand = (hasher.finish() - 2) % 6;
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
+            let material = match rand {
+                0 => material_emissive1.clone(),
+                1 => material_emissive2.clone(),
+                2 => material_emissive3.clone(),
+                3 | 4 | 5 => material_non_emissive.clone(),
+                _ => unreachable!(),
+            };
+
+            commands.spawn(PbrBundle {
+                mesh: mesh.clone(),
+                material,
+                transform: Transform::from_xyz(x as f32 * 2.0, 0.0, z as f32 * 2.0),
+                ..default()
+            });
+        }
     }
 }
