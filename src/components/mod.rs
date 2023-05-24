12e9c892a0c85355015151e28047e5d61932f0b4
stephen <stephen.buchanan@random.studio>
2023-05-24 09:53:45 +0200

    refactor for better structure

---
+++ src/components/mod.rs
@@ -0,0 +1,21 @@
+use bevy::prelude::*;
+
+// Define a struct to keep some information about our entity.
+// Here it's an arbitrary movement speed, the spawn location, and a maximum distance from it.
+#[derive(Component)]
+pub struct Movable {
+    pub spawn: Vec3,
+    pub max_distance: f32,
+    pub speed: f32,
+}
+
+// Implement a utility function for easier Movable struct creation.
+impl Movable {
+    pub fn new(spawn: Vec3) -> Self {
+        Movable {
+            spawn,
+            max_distance: 5.0,
+            speed: 2.0,
+        }
+    }
+}
